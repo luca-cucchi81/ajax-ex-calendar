8e111bce9eada1ceaa2a3b8d65848aaf51d0321d
Luca <lu.cucchi@gmail.com>
2020-04-03 15:51:45 +0200

    js in progress

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
 
 var dataAttuale = moment('2018-01-01');
-console.log(dataAttuale);
+
 
 // al click su precedente --> attiva mese precedente
 $('.prec').click(function(){
     dataAttuale.subtract(1, 'month');
-    console.log(dataAttuale);
+    getGiorni(dataAttuale);
 });
 
 // al click su successivo --> attiva mese successivo
 $('.succ').click(function(){
     dataAttuale.add(1, 'month');
-    console.log(dataAttuale);
+    getGiorni(dataAttuale);
 });
+
+// FUNZIONI
+
+function getGiorni(target){
+    $('#calendar').empty();
+    var meseTarget = target.clone();  //clono il mese attuale
+    var giorniTarget = meseTarget.daysInMonth(); //dal mese attuale estrapolo il numero di giorni 
+    var nomeMese = target.format('MMMM'); //cambio nome mese dopo click
+    $('#nome-mese').text(nomeMese); //
+}
 
 
 
