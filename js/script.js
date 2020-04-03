42ac83bc12842c7c5502eb8f17a6833074c74d85
Luca <lu.cucchi@gmail.com>
2020-04-03 15:08:02 +0200

    js in progress

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
+var dataAttuale = moment('2018-01-01');
+console.log(dataAttuale);
+
+// al click su precedente --> attiva mese precedente
+$('.prec').click(function(){
+    dataAttuale.subtract(1, 'month');
+    console.log(dataAttuale);
+});
+
+// al click su successivo --> attiva mese successivo
+$('.succ').click(function(){
+    dataAttuale.add(1, 'month');
+    console.log(dataAttuale);
+});
 
 
 
@@ -15,5 +29,5 @@ $(document).ready(function(){
 
 
 
-    
+
 });
